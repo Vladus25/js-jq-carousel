26f776a416210b0e5266814f0f57135c975d80a3
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-03-22 16:39:16 +0200

    Cerchi funzionanti. Fine

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 function prevClick(){
 
   var activeImg = $('.active');
+  var activeCircle = $('.activeCic')
 
   if (activeImg.hasClass('first')) {
 
@@ -18,11 +19,30 @@ function prevClick(){
 
   }
 
+  // Cerchi girano insieme con le foto
+  if (activeCircle.hasClass('first')) {
+
+    var prevLi = $('li.last');
+    activeCircle.removeClass('activeCic');
+    prevLi.addClass('activeCic');
+
+  }
+  else {
+
+    var prevLi = activeCircle.prev('li');
+    activeCircle.removeClass('activeCic');
+    prevLi.addClass('activeCic');
+
+  }
+
+
+
 }
 
 function nextClick(){
 
   var activeImg = $('.active');
+  var activeCircle = $('.activeCic')
 
   if (activeImg.hasClass('last')) {
 
@@ -36,6 +56,22 @@ function nextClick(){
     var nextImg = activeImg.next('img');
     activeImg.removeClass('active');
     nextImg.addClass('active');
+
+  }
+
+  // Cerchi girano insieme con le foto
+  if (activeCircle.hasClass('last')) {
+
+    var nextLi = $('li.first');
+    activeCircle.removeClass('activeCic');
+    nextLi.addClass('activeCic');
+
+  }
+  else {
+
+    var nextLi = activeCircle.next('li');
+    activeCircle.removeClass('activeCic');
+    nextLi.addClass('activeCic');
 
   }
 
