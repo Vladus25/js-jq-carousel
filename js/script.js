9b82bd7fc21691e0d3f8a26d39fb605983570e12
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-03-22 14:41:27 +0200

    Esercizio funzionante.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 
 function prevClick(){
 
+  var activeImg = $('.active');
+
+  if (activeImg.hasClass('first')) {
+
+    var prevImg = $('img.last');
+    activeImg.removeClass('active');
+    prevImg.addClass('active');
+
+  }
+  else {
+
+    var prevImg = activeImg.prev('img');
+    activeImg.removeClass('active');
+    prevImg.addClass('active');
+
+  }
+
 }
 
 function nextClick(){
+
+  var activeImg = $('.active');
+
+  if (activeImg.hasClass('last')) {
+
+    var nextImg = $('img.first');
+    activeImg.removeClass('active');
+    nextImg.addClass('active');
+
+  }
+  else {
+
+    var nextImg = activeImg.next('img');
+    activeImg.removeClass('active');
+    nextImg.addClass('active');
+
+  }
 
 }
 
 function init() {
 
   $('.next').click(nextClick);
-  $('.prex').click(prevClick);
+  $('.prev').click(prevClick);
 
 }
 
